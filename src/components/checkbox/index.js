765388c391e964871a7222e262733eea8acd40d3
lumenwrites <lumenwrites@gmail.com>
2022-03-27 03:15:29 +0100

    Fix readOnly checkbox warning.

--- src/components/checkbox/index.js
+++ src/components/checkbox/index.js
@@ -6,7 +6,7 @@ import * as colors from '../../colors'
 export default function Checkbox({ id, name, checked, label, onClick }) {
   return (
     <CheckboxCont onClick={(e) => onClick(e.target.checked)}>
-      <input type="checkbox" id={id} name={name} checked={checked}></input>
+      <input type="checkbox" id={id} name={name} checked={checked} readOnly></input>
       <label htmlFor={id}>{label}</label>
     </CheckboxCont>
   )
